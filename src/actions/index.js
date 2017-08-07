a4e950840aa5a8b232c5196c0d5e570ffcfea296
Paul-Emile <pejoessel@gmail.com>
2017-08-07 08:27:05 +0200

    Added Action Types

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,8 @@ export const SESSION_LOGOUT = 'SESSION_LOGOUT';
 // Navbar
 export const PREVIOUS_DATE = 'PREVIOUS_DATE';
 export const NEXT_DATE = 'NEXT_DATE';
+
+// Sidebar
+export const SIDEBAR_ACTIVATE = 'SIDEBAR_ACTIVATE';
+export const SIDEBAR_ENABLE = 'SIDEBAR_ENABLE';
+export const SIDEBAR_RESPONSIVE = 'SIDEBAR_RESPONSIVE';
