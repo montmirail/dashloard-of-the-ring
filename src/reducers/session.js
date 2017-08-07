891d28cc318a1a5e4139c4cb187816fce8e7abc9
Paul-Emile <pejoessel@gmail.com>
2017-08-07 08:24:06 +0200

    Session_login and Session_Load reducers

--- src/reducers/session.js
+++ src/reducers/session.js
@@ -2,6 +2,15 @@ import { SESSION_LOAD, SESSION_LOGIN, SESSION_LOGOUT } from '../actions';
 
 export default (state = {}, action = {}) => {
     switch (action.type) {
+        case SESSION_LOAD:
+            return action.payload;
+        case SESSION_LOGIN:
+            if (!action.error) {
+                return action.payload
+            } else {
+                console.log(action.payload);
+                return { error: action.payload.message }
+            }
         default:
             return state;
     }
